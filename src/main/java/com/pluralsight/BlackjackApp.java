a5bf5275ea98c9d0d78dc5cf231b5f73db2c4c58
Kendall Dillon <90075443+kadillon21@users.noreply.github.com>
2026-05-07 15:33:19 -0500

    Reformatted code to use less variables

--- src/main/java/com/pluralsight/BlackjackApp.java
+++ src/main/java/com/pluralsight/BlackjackApp.java
@@ -2,27 +2,25 @@ package com.pluralsight;
 
 import com.pluralsight.util.UserInput;
 
+import java.util.ArrayList;
+
 public class BlackjackApp {
 
     public static void main(String[] args) {
 
         boolean addPlayer = true;
-        int totalPlayers = 1;
+        ArrayList<String> players = new ArrayList<>();
 
-        while (addPlayer && totalPlayers <= 7) {
-            UserInput.promptForString("Enter player " + totalPlayers + "'s name ");
+        while (addPlayer && players.size() <= 7) {
+            players.add(UserInput.promptForString("Enter Player " + (players.size() + 1) + "'s name "));
 
-            if (totalPlayers == 7) {
-                return;
-            } else
-                switch (UserInput.promptForChar("Add another player?", "YN")) {
-                    case 'Y':
-                        totalPlayers += 1;
-                        break;
-                    case 'N':
-                        addPlayer = false;
-                        break;
+            if (!(players.size() == 7)) {
+                if (UserInput.promptForChar("Add another player? [Y/N] ", "YN") == 'N') {
+                    addPlayer = false;
+                }
             }
+
+            System.out.println(players.toString());
         }
     }
 }
