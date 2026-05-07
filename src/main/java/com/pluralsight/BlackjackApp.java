e2efd38f3db6ac15b1a50c5e7e639fa43df62b8b
Kendall Dillon <90075443+kadillon21@users.noreply.github.com>
2026-05-07 15:15:47 -0500

    Program now prompts user for names up to 7 names

--- src/main/java/com/pluralsight/BlackjackApp.java
+++ src/main/java/com/pluralsight/BlackjackApp.java
@@ -1,4 +1,29 @@
 package com.pluralsight;
 
+import com.pluralsight.util.UserInput;
+
 public class BlackjackApp {
+
+    public static void main(String[] args) {
+
+        boolean addPlayer = true;
+        int totalPlayers = 1;
+
+        while (addPlayer && totalPlayers <= 7) {
+            UserInput.promptForString("Enter player " + totalPlayers + "'s name ");
+
+            if (totalPlayers == 7) {
+                return;
+            } else
+                switch (UserInput.promptForChar("Add another player?", "YN")) {
+                    case 'Y':
+                        totalPlayers += 1;
+                        break;
+                    case 'N':
+                        addPlayer = false;
+                        break;
+            }
+        }
+    }
 }
+
